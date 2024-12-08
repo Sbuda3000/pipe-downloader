a1abec00ba30751873068bdba1f40469377cf6d8
Sbuda3000 <sbuda3000@proton.me>
2024-12-08 18:16:09 +0200

    Adding heroku config and new yt_dlp package removed pytube

--- app.py
+++ app.py
@@ -1,18 +1,70 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+from pytube import YouTube
+import yt_dlp
+import os
+import re
 
 app = Flask(__name__)
+
+CORS(app)
+
+# Directory where the video will be saved (temporary storage)
+DOWNLOAD_DIRECTORY = "./downloads"
+
+# Create a folder to store the downloaded videos
+DOWNLOAD_FOLDER = 'C:\\\\Users\\mo5623\\Downloads\\youtube-downloader\\downloads\\'
+print(DOWNLOAD_FOLDER)
+if not os.path.exists(DOWNLOAD_FOLDER):
+    os.makedirs(DOWNLOAD_FOLDER)
+
+def sanitize_title(title):
+    return re.sub(r'[\/:*?"<>|]', '_', title)
 
 @app.route('/')
 def home():
     return "Hello from Flask!"
 
-@app.route('/data', methods=['POST'])
-def get_data():
-    data = request.json
-    return jsonify({
-        "status": "success",
-        "data_received": data
-    })
+@app.route('/download', methods=['POST'])
+def download_video():
+    try:
+        # Get the JSON data from the request
+        data = request.get_json()
+
+        # Check if the 'url' field is in the JSON data
+        if 'url' not in data or data['url'].strip() == '':
+            return jsonify({
+                "error": "No URL provided"
+            }), 400
+
+        youtube_url = data['url'].strip()
+        print(f"Downloading video from: {youtube_url}")
+
+        # Use yt-dlp to download the video
+        option ={
+            'format': 'bestaudio/best',
+            'outtmpl': os.path.join(DOWNLOAD_FOLDER, '%(title)s.%(ext)s'),
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+
+        with yt_dlp.YoutubeDL(option) as ydl:
+            info = ydl.extract_info(youtube_url, download=True)
+            file_path = os.path.join(DOWNLOAD_FOLDER, f"{sanitize_title(info['title'])}.mp3")
+            print(file_path)
+
+        return jsonify({
+            "file_path": file_path
+        }), 200
+    
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return jsonify({
+            "error": str(e)
+        }), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
-// import 'dart:convert';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,7 +16,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter Python Integration",
+      title: "Pipe Downloader",
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -27,32 +29,101 @@ class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
-  _HomePageState createState() => _HomePageState();
+  HomePageState createState() => HomePageState();
 }
 
- class _HomePageState extends State<HomePage> {
-  String _response = "Response will appear here";
+ class HomePageState extends State<HomePage> {
+  final TextEditingController _urlController = TextEditingController();
+  String _status = "Enter a YouTube URL";
+  String _filePath = "";
 
-  Future<void> _sendDataToPython() async {
-    // Send a POST request with some sample data.
-    final url = Uri.parse('http://127.0.0.1:5000/data');
-    final response = await http.post(
-      url, 
-      headers: {"Content-Type": "application/json"},
-      body: jsonEncode({
-        "name": "Flutter",
-        "message": "Hello, Python!"
-      }));
+  // Function to request storage permission
+  Future<bool> _requestStoragePermission() async {
+    var status = await Permission.storage.status;
 
-    if (response.statusCode == 200) {
+    if (status.isDenied) {
+      // Request permission
+      status = await Permission.storage.request();
+    }
+
+    if (status.isPermanentlyDenied) {
+      // The user has denied the permission permanently, direct them to settings
+      openAppSettings();
+      return false;
+    }
+
+    return status.isGranted;
+  } 
+
+  Future<void> _downloadYouTubeVideo() async {
+    // Check if the URL is non-empty
+    final url = _urlController.text;
+
+    if (url.isEmpty) {
+        setState(() {
+          _status = "Please enter a valid YouTube URL";
+        });
+        return;
+      }
+
+    // Request storage permission before downloading the video
+    bool permissionGranted = await _requestStoragePermission();
+
+    if (!permissionGranted) {
       setState(() {
-        _response = response.body;
+        _status = "Storage permission denied";
+      });
+      return;
+    }
+
+    try {
+      setState(() {
+        _status = "Downloading video...";
+      });
+
+      // Make a POST request to the Flask backend to download the video
+      final response = await http.post(Uri.parse('http://10.0.2.2:5000/download'),
+                                      headers: {"Content-Type": "application/json"},
+                                      body: jsonEncode({"url": url})); // Pass the YouTube URL here
+      
+      if (response.statusCode == 200) {
+        final jsonResponse = jsonDecode(response.body);
+        final downloadedFilePath = jsonResponse['file_path'];
+
+        // Move downloaded file to app's local storage
+        await _saveFileToLocalDirectory(downloadedFilePath);
+
+        setState(() {
+          _status = "Download complete!";
+          _filePath = downloadedFilePath;
+        });
+      }
+      else {
+        setState(() {
+          _status = "Error: ${response.body}";
+        });
+      }
+    }
+    catch (e) {
+      setState(() {
+        _status = "Error occurred: $e";
       });
     }
-    else {
+  }
+
+  Future<void> _saveFileToLocalDirectory(String downloadedFilePath) async {
+    final directory = await getExternalStorageDirectories();
+    if (directory != null) {
+      final fileName = downloadedFilePath.split('/').last;
+      // ignore: unused_local_variable
+      final savedFile = File('$directory/$fileName');
+
+      // Move the file from temp download directory to app's local storage
+      final tempFile = File(downloadedFilePath);
+      await tempFile.copy(savedFile.path);
+
       setState(() {
-        _response = "Error: ${response.statusCode}";
+        
       });
     }
   }
@@ -61,19 +132,29 @@ class HomePage extends StatefulWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Flutter Python Integration"),
+        title: const Text("Pipe Downloader"),
       ),
-      body: Center(
+      body:  Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(_response),
+            TextField(
+              controller: _urlController,
+              decoration: const InputDecoration(
+                labelText: "YouTube URL",
+                border: OutlineInputBorder(),
+              ),
+            ),
             const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: _sendDataToPython, 
-              child: const Text('Send Data to Python'))
-          ],
-        ),
+              onPressed: _downloadYouTubeVideo, 
+              child: const Text("Download Video"),
+            ),
+            const SizedBox(height: 20),
+            Text(_status),
+            _filePath.isNotEmpty ? Text("Saved at: $_filePath") : Container(),
+          ],),
       ),
     );
   }
